494bee64cb3b264c2bf2309afa2406d6eceaac76
Mustafa Yusuf <84468171+theeyusuf07@users.noreply.github.com>
2022-01-02 14:21:02 +0300

    Update sea.cpp

--- sea.cpp
+++ sea.cpp
@@ -1,1 +1,44 @@
-# include <iostream>
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+	/* 
+	5 tane degisken vardır birinicisi  int 
+	cout ile tanımlanan sayıyı ekrana yazdırıyoruz 
+	
+	int ilkSayi=15;
+	cout<<"ilk Sayi: "<<ilkSayi;
+	
+	int ilksayi=12;
+	double ikincisayi=3.50;
+	
+	cout<<"ilk sayi: "<<ilksayi<<endl
+	cout<<"ikinci sayi: "<<ikincisayi<<endl;
+	
+*/
+	
+	//metinsel degiskenler
+	
+//	string isim="Mustafa",soyisim="Yigit";
+//	int yas=19,dogyil=2002;
+	
+//	cout<<"isim: "<<isim<<endl;
+//	cout<<"soyisim: "<<soyisim<<endl;
+	
+//	cout<<"Yas: "<<yas<<endl<<"Dogum Yili: "<<dogyil;
+	
+	//BOOL DEGİSKENİ
+	// true yada false degiskenlerini tutan bir türdür
+	
+//	bool admin=false;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
